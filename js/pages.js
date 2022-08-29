fe88eed7d00bc385e82288fb1096cfd2bc868a60
Anthony Folino <91862159+a-folino@users.noreply.github.com>
2022-08-29 19:24:33 +1000

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -5,7 +5,7 @@ let url = `https://newsapi.org/v2/top-headlines?country=au&category=${category}&
 const fetchNews = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    return newsArticles(data);
+    return setTimeout(() => {newsArticles(data)}, 600);
 }
 
 fetchNews();
